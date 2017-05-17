a0da4ecf4bb8413b7dcbe2e18fcecaf88c5d53cc
wwwins <jacky.huang@isobar.com>
2017-05-17 11:49:11 +0800

    Added support apng - addAnim - speedupAll - slowdownAll

--- index.js
+++ index.js
@@ -42,10 +42,17 @@ $(function(){
     }
     if (cmd=='addvideo') {
       vsno++;
-      addVideo("video"+vsno);
+      // addVideo("video"+vsno);
+      addAnim("video"+vsno);
     }
     if (cmd=='rmvideo') {
       removeVideo();
+    }
+    if (cmd=='speedup') {
+      speedupAll();
+    }
+    if (cmd=='slowdown') {
+      slowdownAll();
     }
   });
 
@@ -82,11 +89,11 @@ $(function(){
   }
 
   function playallSlower() {
-    if (speedRate > 1.0) {
+    if (speedRate <= 1.0) {
       speedRate = 1.0
     }
     else {
-      speedRate = speedRate + PlayRate;
+      speedRate = speedRate - PlayRate;
     }
     $('video').each(function(){$(this).playbackRate = speedRate;});
     // $('video').get(0).playbackRate = speedRate;
@@ -98,14 +105,32 @@ $(function(){
   }
 
   function playSlower(vid) {
-    if (speedRate > 1.0) {
+    if (speedRate <= 1.0) {
       speedRate = 1.0
     }
     else {
-      speedRate = speedRate + PlayRate;
+      speedRate = speedRate - PlayRate;
     }
     // $('#').each(function(){$(this).playbackRate = speedRate;});
     $('#'+vid).get(0).playbackRate = speedRate;
+  }
+
+  // <div class="level3">
+  //   <img src="image/animated24.png" width="640" height="360">
+  // </div>
+  function randDiv(n) {
+    var video_w = 640;
+    var video_h = 360;
+
+    for (var i = 1; i<n ; i++) {
+      var vid = "video" + i;
+      var px = (Math.random() * ($(document).width() - video_w)).toFixed();
+      var py = (Math.random() * 100).toFixed();
+      var randw = ((video_w * 0.5) + Math.random() * (video_w * 0.5)).toFixed();
+      var randh = (randw * (360/640)).toFixed();
+      $('.container').append('<div style="position:absolute;left:'+px+'px;bottom:'+py+'px"><img id="'+vid+'" width="'+randw+'" height="'+randh+'" src="image/animated24.png"></div>');
+    }
+    $('img').hide();
   }
 
   // hideAll();
@@ -113,6 +138,28 @@ $(function(){
   //   addVideo("video"+i);
   // }
 
+  // APNG
+  // APNG.ifNeeded().then(function () {
+  //   $(".container").find("img").each(function () {
+  //     $(this).show();
+  //     APNG.animateImage(this);
+  //   });
+  // })
+
+  // APNG.parseURL("image/elephant.png").then(function(anim){
+  //   var canvas = document.querySelector('#canvas_apng');
+  //   anim.addContext(canvas.getContext("2d"));
+  //   anim.playbackRate = 1.0;
+  //   anim.play();
+  //   console.log("playTime:"+anim.playTime);
+  // });
+  // 類似上面做法
+  APNG.animateContext("image/elephant.png", document.querySelector('#canvas_apng').getContext("2d")).then(function(anim){
+    anim.playbackRate = 1.5;
+  });
+
+  // 先產生 20 個 div/img
+  randDiv(20);
 });
 
 // 加入影片
@@ -124,4 +171,39 @@ function addVideo(vid) {
   var randw = ((video_w * 0.5) + Math.random() * (video_w * 0.5)).toFixed();
   console.log("("+px+","+py+"),"+randw);
   $('.container').append('<div style="position:absolute;left:'+px+'px;bottom:'+py+'px"><video id="'+vid+'" width="'+randw+'" src="video/test2M.webm" autoplay="" loop=""></video></div>');
+}
+
+var video_map = new Map();
+function addAnim(aid) {
+  console.log("addAnim:"+aid);
+  APNG.ifNeeded().then(function () {
+    $('#'+aid).show();
+    return APNG.animateImage($('#'+aid)[0]);
+    console.log("animated");
+  }).then(function (anim) {
+    // anim.playbackRate = 1.5;
+    // hash[aid] = anim;
+    video_map.set(aid, anim);
+  })
+}
+
+var PlayRate = 0.25;
+var speedRate = 1.0;
+function speedupAll() {
+  speedRate = speedRate + PlayRate;
+  video_map.forEach(function(value,key){
+    value.playbackRate = speedRate;
+  });
+}
+
+function slowdownAll() {
+  if (speedRate <= 1.0) {
+    speedRate = 1.0
+  }
+  else {
+    speedRate = speedRate - PlayRate;
+  }
+  video_map.forEach(function(value,key){
+    value.playbackRate = speedRate;
+  });
 }
